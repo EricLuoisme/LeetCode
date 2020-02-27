cf3ce45ae43b8ed13aedfa5a323880bf1b062dc7
EricLuo <lykismekk@outlook.com>
2020-02-27 20:51:38 +0800

    slightly change

--- Ex17_LetterCombinationsOfPhoneNumber/LetterCombinations.py
+++ Ex17_LetterCombinationsOfPhoneNumber/LetterCombinations.py
@@ -18,25 +18,6 @@ def letterCombinations(digits: str):
     dic[8] = {'t', 'u', 'v'}
     dic[9] = {'w', 'x', 'y', 'z'}
 
-    # i = 2 # slower
-    # n = 1
-    # mod_num = 3
-    # the_set = []
-    #
-    # # build the dictionary
-    # for a in ascii_lowercase:
-    #     the_set.append(a)
-    #     if i == 7 or i == 9:
-    #         mod_num = 4
-    #     else:
-    #         mod_num = 3
-    #     if n % mod_num == 0:
-    #         dic[i] = the_set
-    #         i += 1
-    #         the_set = []
-    #         n = 0
-    #     n += 1
-
     # get cartesian product
     temp_lists = []
     for c in digits:
